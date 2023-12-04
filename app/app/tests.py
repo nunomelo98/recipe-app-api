12c880d1bc56d805fb13921eb5910623eeb6f59a
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2023-12-04 16:18:27 -0100

    lint error resolved

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,7 @@
 """
 Sample tests
 """
-from django.test        import SimpleTestCase
+from django.test import SimpleTestCase
 
 from app import calc
 
@@ -10,11 +10,11 @@ class CalcTests(SimpleTestCase):
     
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         
         self.assertEqual(res, 11)
         
     def test_subtr(self):
         """Test subtraction of two numbers."""
-        res = calc.subtract(10,15)
-        self.assertEqual(res,5)
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
