fff198d5147506e526ddc7231e9dcc1e59bffab8
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2023-12-04 16:28:32 -0100

    adding db setteings

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,6 +6,6 @@ def add(x, y):
     """Add x and y return result."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y and return result"""
-    return y-x
+    return y - x
