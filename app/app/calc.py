87ad691c135bd813f6a93e9809ba524b5dee8679
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2024-07-07 22:34:29 -0100

    update main chances

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
-Calculator fucntions
+Calculator functions
 """
+
 
 def add(x, y):
     """Add x and y return result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result"""
