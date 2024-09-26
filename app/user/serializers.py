a71337f9ac1e56b70b8768771ab20d5dd89f617c
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2024-09-26 17:52:09 -0100

    feat:lint error resolved

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,8 +5,10 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
